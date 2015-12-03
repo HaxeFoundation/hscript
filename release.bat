47652825fca10ec1f153db9e2d2a07b2f39d4d2b
Sebastian Thomschke <sebthom@users.noreply.github.com>
2015-12-03 17:12:27 +0100

    added run script

--- release.bat
+++ release.bat
@@ -6,6 +6,12 @@ cd release
 mkdir hscript
 cd ..
 cp hscript/*.hx release/hscript
+cd release
+mkdir script
+cp script/*.hx* release/script
+cd release/script
+haxe build.hxml
+cd ../..
 haxe -xml release/haxedoc.xml hscript.Interp hscript.Parser hscript.Bytes hscript.Macro
 7z a -tzip release.zip release
 rm -rf release
